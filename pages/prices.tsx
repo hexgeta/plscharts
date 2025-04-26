9fa321c3c6141670c393a77146cfa5db6eda17c4
hexgeta <hexgeta@proton.me>
2025-04-26 09:13:39 +0100

    auto-commit

--- pages/prices.tsx
+++ pages/prices.tsx
@@ -8,7 +8,7 @@ const PricesPage = () => {
   return (
     <div className="p-2 sm:p-4">
       <h1 className="text-2xl font-bold mt-10 mb-4 text-center">Price Charts</h1>
-      <p className="text-white/60 text-center">A chart showing pooled asset prices in a single view.</p>
+      <p className="text-white/60 text-center">A chart showing pooled asset prices in a single view</p>
       <div>
         {/* <PriceChart/> */}
         <PriceChartPLS/>
