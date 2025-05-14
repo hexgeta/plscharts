a35e847d8e70d786405293a9b9aad674eed28b62
hexgeta <hexgeta@proton.me>
2025-05-14 16:49:03 +0100

    auto-commit

--- next.config.js
+++ next.config.js
@@ -38,12 +38,12 @@ const nextConfig = {
       },
       {
         source: '/live',
-        destination: 'https://x.com/i/broadcasts/1jMKgkoAvmjKL',
+        destination: 'https://x.com/i/broadcasts/1rmGPyPnVgDJN',
         permanent: false,
       },
       {
         source: '/livestream',
-        destination: 'https://x.com/i/broadcasts/1jMKgkoAvmjKL',
+        destination: 'https://x.com/i/broadcasts/1rmGPyPnVgDJN',
         permanent: false,
       },
       
